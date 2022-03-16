7adbaf124e6b42cac5a9978f4da3203da052240f
Jon Kaplon <jon.kaplon1@gmail.com>
2022-03-16 14:39:45 -0700

    commit tree ac6e1fe5044cc92c64def7a9dbe8b65c861015d7

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
 
+class CashRegister
+    attr_accessor :total, :discount, :last_transaction
+
+    # def initialize(discount=0)
+    #     @total = 0
+    #     @discount = discount
+    # end
+
+    # def add_item(title, price, quantity=0)
+    #     title.
+    # end
+end
